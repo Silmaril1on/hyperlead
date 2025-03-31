0e6283689c3b9c433d96230ba796b0dda7f64e27
Graph Levan Chiqovani <graphchiqovani@yahoo.com>
2025-03-31 21:53:57 +0400

    change

--- src/app/(routes)/changelog/page.js
+++
@@ -1,9 +0,0 @@
-const ChangeLogPage = () => {
-  return (
-    <div>
-      <h1>hello from ChangeLogPage</h1>
-    </div>
-  );
-};
-
-export default ChangeLogPage;
--- src/pages/homepage/HomePage.jsx
+++ src/pages/homepage/HomePage.jsx
@@ -1,3 +1,5 @@
+"use client";
+import { useEffect, useState } from "react";
 import LogoSlider from "./LogoSlider";
 import SectionFaq from "./sectionFAQ/SectionFaq";
 import SectionGlobe from "./sectionGlobe/SectionGlobe";
@@ -5,8 +7,26 @@ import SectionOne from "./sectionOne/SectionOne";
 import SectionPricing from "./sectionPricing/SectionPricing";
 import SectionThree from "./sectionThree/SectionThree";
 import SectionTwo from "./sectionTwo/SectionTwo";
+import supabase from "@/config/supabaseClient";
 
 const HomePage = () => {
+  const [leads, setLeads] = useState([]);
+
+  useEffect(() => {
+    const fetchLeads = async () => {
+      const { data, error } = await supabase.from("leads").select();
+      if (error) {
+        console.error("Error fetching leads:", error);
+      } else {
+        setLeads(data);
+      }
+    };
+
+    fetchLeads();
+  }, []);
+
+  console.log(leads);
+
   return (
     <main className="center flex-col space-y-20">
       <SectionOne />
@@ -16,6 +36,17 @@ const HomePage = () => {
       <SectionPricing />
       <SectionFaq />
       <SectionGlobe />
+      <div className="h-screen center w-full">
+        {leads.length > 0 ? (
+          <ul>
+            {leads.map((lead, index) => (
+              <li key={index}>{lead.name}</li>
+            ))}
+          </ul>
+        ) : (
+          "Loading..."
+        )}
+      </div>
     </main>
   );
 };
